3fe92cd005cc4c6393cda5e51b0f52232ae74292
abdullah01 <arfathossen541@gmail.com>
2024-01-22 11:15:48 +0600

    langing page completed

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// @type {import('tailwindcss').Config}
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+
+  plugins: [],
+};
